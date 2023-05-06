d1931bbc0d9c04fee59e2456d52bcb7a78d15a54
Jerell James <jerell1998@gmail.com>
2023-05-06 08:31:59 +0100

    pre-commit

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -2,6 +2,6 @@ export default {
   // Prettiers supported file list is too long,
   // so just throw everything at it and let it
   // decide what not to read using -u
-  '**/*': ['npx prettier -w -u'],
-  'frontend/src/**/*': () => ['npx -w frontend next lint --fix'],
+  '**/*': ['npx prettier -u'],
+  'frontend/src/**/*': () => ['npm run pretty'],
 };
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-around p-24'>
-      <h1 className='text-xl'>jigsaw</h1>
+      <h1 className='text-xl font-bold'>jigsaw</h1>
       <div className='mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left'>
         <a
           href='https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'

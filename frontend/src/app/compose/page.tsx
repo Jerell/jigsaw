86b3acd58f663a63353a1c4254188831930fc291
Jerell James <jerell1998@gmail.com>
2023-05-06 16:05:37 +0100

    cylinder

--- frontend/src/app/compose/page.tsx
+++ frontend/src/app/compose/page.tsx
@@ -1,8 +1,9 @@
+import CylinderScene from '@/components/plot/three/Scene';
+
 export default function Compose() {
   return (
     <section className='flex flex-col items-center justify-around grow'>
       <p>compose</p>
-      <div className='flex flex-col items-center gap-2'></div>
     </section>
   );
 }
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,11 @@
+import CylinderScene from '@/components/plot/three/Scene';
+
 export default function Home() {
   return (
     <main className='flex flex-col items-center justify-around'>
-      <p>hello</p>
+      <div className='h-96 w-full'>
+        <CylinderScene />
+      </div>
     </main>
   );
 }
